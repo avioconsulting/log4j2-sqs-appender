10f3e048bb8a8e298337c2d72bb51a8647ffb569
Adam Mead <amead@avioconsulting.com>
2022-03-20 20:55:15 -0500

    Split large messages into chunks before sending to SQS

--- Readme.md
+++ Readme.md
@@ -55,6 +55,35 @@ Add this custom appender to your Root logger in log4j2.xml.
     <AppenderRef ref="SQS" />
 </AsyncRoot>
 ```
+
 * That's it!
 
 When you run the project with this appender added with your AWS credentials, you should the see your app logs flowing into the specified SQS queue.
+
+Optional Configurations
+=======================
+* `largeMessagesEnabled` - set this to `true` to enable large message handling. Defaults to `false`
+* `largeMessageQueueName` - this is the name of an SQS FIFO queue to be used for large messages. If the attribute is not provided `.fifo` will be appended to the `queueName` attribute
+* `maxMessageBytes` - this is the maximum size to allow for a single message on the SQS queue. SQS has a maximum message size of 256 KB, so the default maximum size is 250 KB to allow for space in the message to provide info needed to rebuild the split message
+
+Example with all possible configurations:
+```
+<Appenders>
+    <SQS name="SQS" 
+         awsAccessKey="<your AWS access key>" 
+         awsRegion="<your AWS region>" 
+         awsSecretKey="<your AWS secret key>" 
+         maxBatchOpenMs="10000" 
+         maxBatchSize="5" 
+         maxInflightOutboundBatches="5" 
+         queueName="<your AWS SQS queue name>"
+         largeMessagesEnabled="true"
+         largeMessageQueueName="<your AWS SQS FIFO queue name>"
+         maxMessageBytes="250">
+        <PatternLayout pattern="%-5p %d [%t] %c: ##MESSAGE## %m%n"/>
+    </SQS>
+</Appenders>
+```
+
+When `largeMessagesEnabled` is true, messages larger than `maxMessageBytes` will be split into multiple SQS messages and placed on a FIFO queue with the same group ID so they are guaranteed to be processed in order. The SQS messages will need to be accumulated so the log message can be reconstructed.
+The message format looks like this: `currentPart=1|totalParts=5|hash=f11b7d310127a11680d0eb73d3d109adfa48608efa67f5fd69876e2f60b4ec22|message="{\"timeMillis\":1647825592778,\"thread\":\"[MuleRuntime].uber.09: [sqs-test].logFilesFlow.CPU_LITE @5d97440f\",\"level\":\"INFO\",\"loggerName\":\"com.avioconsulting.api\",\"message\":{\"timestamp\":\"2022-03-21T01:19:52.776Z\",\"appName\":\"sqs-test\",\"appVersion\":\"1.0.0\",\"correlationId\":\"0295a030-a8b5-11ec-95cb-f01898a624f3\",\"payload\":\"{\\n  \\\"filePath\\\": \\\"/docs/samplePayload.json\\\"\\n}\"},\"endOfBatch\":true,\"loggerFqcn\":\"org.apache.logging.log4j.spi.AbstractLogger\",\"contextMap\":{\"correlationId\":\"0295a030-a8b5-11ec-95cb-f01898a624f3\",\"processorPath\":\"logFilesFlow/processors/0/processors/0\"},\"threadId\":58,\"threadPriority\":5,\"timestamp\":\"2022-03-20T20:19:52.778-0500\",\"deployedAppName\":\"${sys:domain}\"}"`
--- src/main/java/com/avioconsulting/log4j/sqs/SqsManager.java
+++ src/main/java/com/avioconsulting/log4j/sqs/SqsManager.java
@@ -7,13 +7,24 @@ import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
 import com.amazonaws.services.sqs.buffered.QueueBufferConfig;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.AbstractManager;
 import org.apache.logging.log4j.core.config.Configuration;
 
-import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 import java.util.Objects;
 
 public class SqsManager extends AbstractManager {
@@ -22,12 +33,17 @@ public class SqsManager extends AbstractManager {
     private final String awsSecretKey;
     private final String awsRegion;
     private final String queueName;
+    private final String largeMessageQueueName;
     private final Integer maxBatchOpenMs;
     private final Integer maxBatchSize;
     private final Integer maxInflightOutboundBatches;
+    private final Integer maxMessageBytes;
+    private final Boolean largeMessagesEnabled;
     private String queueUrl;
+    private String largeMessageQueueUrl;
     private AmazonSQSBufferedAsyncClient client;
 
+    private static final Logger logger = LogManager.getLogger();
 
     protected SqsManager(final Configuration configuration,
                          final LoggerContext loggerContext,
@@ -36,18 +52,24 @@ public class SqsManager extends AbstractManager {
                          final String awsAccessKey,
                          final String awsSecretKey,
                          final String queueName,
+                         final String largeMessageQueueName,
                          final Integer maxBatchOpenMs,
                          final Integer maxBatchSize,
-                         final Integer maxInflightOutboundBatches) {
+                         final Integer maxInflightOutboundBatches,
+                         final Integer maxMessageBytes,
+                         final Boolean largeMessagesEnabled) {
         super(loggerContext, name);
         this.configuration = Objects.requireNonNull(configuration);
         this.awsAccessKey = awsAccessKey;
         this.awsSecretKey = awsSecretKey;
         this.awsRegion = awsRegion;
         this.queueName = queueName;
+        this.largeMessageQueueName = largeMessageQueueName == null ? queueName.concat(".fifo") : largeMessageQueueName;
         this.maxBatchOpenMs = maxBatchOpenMs == null ? 200 : maxBatchOpenMs;
         this.maxBatchSize = maxBatchSize == null ? 10 : maxBatchSize;
         this.maxInflightOutboundBatches = maxInflightOutboundBatches == null ? 5 : maxInflightOutboundBatches;
+        this.maxMessageBytes = maxMessageBytes == null ? 250000 : maxMessageBytes;
+        this.largeMessagesEnabled = largeMessagesEnabled != null && largeMessagesEnabled;
     }
 
     public Configuration getConfiguration() {
@@ -58,39 +80,108 @@ public class SqsManager extends AbstractManager {
         // This default implementation does nothing
     }
 
-    public void send(final Layout<?> layout, final LogEvent event) throws IOException {
+    public void send(final Layout<?> layout, final LogEvent event) {
         try {
-            String message = new String(layout.toByteArray(event), "UTF-8");
-            SendMessageRequest request = new SendMessageRequest(this.queueUrl, message);
-            this.getClient().sendMessageAsync(request);
+            String message = new String(layout.toByteArray(event), StandardCharsets.UTF_8);
+            int messageLength = message.getBytes().length;
+            logger.debug("Message length: " + messageLength);
+
+            // We only need to split the message if it's larger than the maximum length
+            if (messageLength > this.maxMessageBytes && this.largeMessagesEnabled) {
+                logger.debug("Splitting large message");
+                // Generate Message Hash to use as the group ID
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                String messageHash = BytesToHex(digest.digest(message.getBytes(StandardCharsets.UTF_8)));
+                logger.debug("Large Message Hash: " + messageHash);
+                //String base64Message = Base64.getEncoder().encodeToString(message.getBytes(StandardCharsets.UTF_8));
+                String[] splitMessage = SplitStringByByteLength(message, "UTF-8", maxMessageBytes);
+                for (int i = 0; i < splitMessage.length; i++) {
+                    logger.debug(String.format("Sending message %d of %d", i + 1, splitMessage.length));
+                    SendMessageRequest request = new SendMessageRequest(this.largeMessageQueueUrl, String.format("currentPart=%d|totalParts=%d|hash=%s|message=%s", i + 1, splitMessage.length, messageHash, splitMessage[i]));
+                    request.setMessageGroupId(messageHash);
+                    this.getClient().sendMessageAsync(request);
+                }
+            } else {
+                SendMessageRequest request = new SendMessageRequest(this.queueUrl, message);
+                this.getClient().sendMessageAsync(request);
+            }
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Failed to send message to SQS");
+            logger.error("Failed to send message to SQS", e);
         }
     }
 
-     private AmazonSQSBufferedAsyncClient getClient() {
-        if(this.client == null) {
+    private AmazonSQSBufferedAsyncClient getClient() {
+        if (this.client == null) {
             AmazonSQSAsync asyncClient;
             try {
-                System.out.println("Initializing SQS Client: " + this.toString());
+                logger.debug("Initializing SQS Client: " + this);
                 AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey));
                 AmazonSQSAsyncClientBuilder clientBuilder = AmazonSQSAsyncClientBuilder.standard();
                 asyncClient = clientBuilder.withRegion(awsRegion).withCredentials(credentialsProvider).build();
                 QueueBufferConfig config = new QueueBufferConfig().withMaxBatchOpenMs(maxBatchOpenMs).withMaxBatchSize(maxBatchSize).withMaxInflightOutboundBatches(maxInflightOutboundBatches);
                 this.client = new AmazonSQSBufferedAsyncClient(asyncClient, config);
                 this.queueUrl = this.client.getQueueUrl(this.queueName).getQueueUrl();
+                if (largeMessagesEnabled) {
+                    logger.debug("Large Messages Enabled. Large Message Queue: " + this.largeMessageQueueName);
+                    this.largeMessageQueueUrl = this.client.getQueueUrl(this.largeMessageQueueName).getQueueUrl();
+                }
             } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("Failed to initialize SQS Client");
+                logger.error("Failed to initialize SQS Client", e);
                 throw e;
             }
         }
 
         return this.client;
-     }
+    }
 
-     public String toString() {
+    public String toString() {
         return "[ region=" + this.awsRegion + ", maxBatchOpenMs=" + this.maxBatchOpenMs + ", maxBatchSize=" + this.maxBatchSize + ", maxInflightOutboundBatches=" + this.maxInflightOutboundBatches + " ]";
-     }
+    }
+
+    /**
+     * Convert a byte array into a String of Hex characters. Using this so we don't havce to add another dependency.
+     *
+     * @param hash  the byte array represending the hash value
+     * @return Hex String
+     */
+    private static String BytesToHex(final byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    /**
+     * Split the input string into a list of strings, each with a maximum memory size of maxsize bytes.
+     *
+     * @param src the source string
+     * @param encoding the encoding to use for the output string
+     * @param maxsize the maximum size of the string parts in bytes
+     * @return an array of strings, each with a maximum length of maxsize bytes
+     */
+    private static String[] SplitStringByByteLength(final String src, final String encoding, final int maxsize) {
+        Charset cs = Charset.forName(encoding);
+        CharsetEncoder coder = cs.newEncoder();
+        ByteBuffer out = ByteBuffer.allocate(maxsize);  // output buffer of required size
+        CharBuffer in = CharBuffer.wrap(src);
+        List<String> ss = new ArrayList<>();            // a list to store the chunks
+        int pos = 0;
+        while (true) {
+            CoderResult result = coder.encode(in, out, true); // try to encode as much as possible
+            int newpos = src.length() - in.length();
+            String s = src.substring(pos, newpos);
+            ss.add(s);                                  // add what has been encoded to the list
+            pos = newpos;                               // store new input position
+            out.rewind();                               // and rewind output buffer
+            if (!result.isOverflow()) {
+                break;                                  // everything has been encoded
+            }
+        }
+        return ss.toArray(new String[0]);
+    }
 }
